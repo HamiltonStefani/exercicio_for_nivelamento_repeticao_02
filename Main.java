3f6e65b3212790af35810201cd14eddb47043c69
HamiltonStefani <88253849+HamiltonStefani@users.noreply.github.com>
2023-01-16 21:47:39 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,22 @@
+package exerciciofor02;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner hs = new Scanner(System.in);
+		
+		for (int i = 0;i < 11; i++) {
+			
+			System.out.println("Valor de i: " + i);
+			
+		}
+		
+			
+			
+
+	}
+
+}
